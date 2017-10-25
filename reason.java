78176594f593448075b9f41861657d2e39942b6a
Nazmul4120 <nazmul4120sarker@gmail.com>
2017-10-26 01:59:40 +0600

    reason.java
    funny code

---
+++ reason.java
@@ -0,0 +1,39 @@
+import static java.lang.System.*;
+
+public class Program
+{
+    public static void main(String[] args) {
+        out.println("REASONS TO DATE A PROGRAMMER");
+        
+        out.println();
+        
+        out.println("1) We are agile & lean.");
+        out.println("2) We can open any jars for you.");
+        out.println("3) We love to kiss(keep it simple, stupid).");
+        out.println("4) We could give you pearl & ruby.");
+        out.println("5) We aren\'t afraid to commit.");
+        out.println("6) We can speak multiple languages.");
+        out.println("7) We are really good at getting rid of bugs.");
+        out.println("8) We can take you to the cloud.");
+        out.println("9) We will never have a null movement.");
+        out.println("10) We know the right keystrokes to turn things on.");
+        
+        out.println("11) We break keyboards, not hearts.");
+        out.println("12) We can handle exceptions too.");
+        out.println("13) We terminate loops, not relationship.");
+        out.println("14) We make things simpler, not complicated(though our work is complicated).");
+        out.println("15) We can deal with any kind of error(problem).");
+        out.println("16) We have good eyesight as we can see sharp(C#).");
+        out.println("17) All you need to do to please us is give some coffee. We can turn it into code.");
+        out.println("18) We declare variables, not wars.");
+        out.println("19) We have sailed the seas(Cs).");
+        out.println("20) We have conquered the deadliest of Pythons.");
+        
+        out.println("21) If we ever abuse you, you will never understand that we are abusing you.");
+        out.println("22) We know the value of cleaning up(garbage collection).");
+        out.println("23) If we can search for a semicolon through countless lines of code, it means we\'ve mastered the art of patience");
+        out.println("24) We can interface with whatever we like.");
+        out.println("25) We execute programs, not people.");
+        out.println("26) We can create cookies for you.");
+    }
+}
